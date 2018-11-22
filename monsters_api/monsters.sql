68569b2547a9edab92a4df173a28ab0b17d53dd1
mukesh <mukeshreddy9992@gmail.com>
2018-11-21 19:36:38 -0700

    folders added

--- monsters_api/monsters.sql
+++ monsters_api/monsters.sql
@@ -9,7 +9,22 @@ CREATE TABLE habitats(
     climate character varying(50),
     temperature int
 );
-CREATE TABLE monsters(
+CREATE TABLE lives(
     monsters character varying(50),
     habitats character varying(50)
 );
+INSERT INTO monsters(name, personality)
+VALUES 
+('fluffy', 'agrresive'),
+('Noddles', 'impatient'),
+('Rusty', 'passionate');
+INSERT INTO habitats(name, climate, temperature)
+VALUES 
+('desert', 'dry', 100),
+('forest', 'haunted', 70 ),
+('mountain', 'icy', 30);
+INSERT INTO lives(monster, habitat)
+VALUES 
+('Fluffy', 'desert'),
+('Noddles', 'forest'),
+('Rusty', 'mountain');
